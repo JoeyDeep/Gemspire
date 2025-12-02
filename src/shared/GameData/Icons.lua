35b2b74e14bada753f15fde985b3e4aa55488ded
joeydeep <grvorce@gmail.com>
2025-12-01 16:11:13 -0800

    toolbar ui hook, PlayerService refactor & server->client networking methods

--- src/shared/GameData/Icons.lua
+++ src/shared/GameData/Icons.lua
@@ -1,5 +1,6 @@
 return {
 	Other = "http://www.roblox.com/asset/?id=15928895003", -- question mark icon
 
+	HoldLoot = "rbxassetid://6788518051",
 	Pickaxe = "http://www.roblox.com/asset/?id=14257565324",
 }
