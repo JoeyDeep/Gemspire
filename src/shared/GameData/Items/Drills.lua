d3f216677c7f1e937c07f4e81d4897e1faea2c3e
matthew gill <matthewgill@matthews-MacBook-Pro.local>
2026-01-07 17:41:39 -0800

    first version of drill. not complete yet. working features: placement, basic collection, basic animation, new RNG Evaluator

---
+++ src/shared/GameData/Items/Drills.lua
@@ -0,0 +1,10 @@
+return {
+	rusty = {
+		name = "Rusty Drill",
+		description = "A rusty drill that has seen better days. It is not very effective but can be used to mine basic ores.",
+		lootInterval = 1, -- 6 minutes 
+		maxLoot = 10,
+		rarity = 1,
+		luck = 1,
+	},
+}
